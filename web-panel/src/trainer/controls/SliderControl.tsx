88750b906c435598aaad3a04162ca2d3045aef2e
Strixzy <natthapon.guitar@gmail.com>
2026-09-04 17:17:04 +0700

    Add files via upload

---
+++ web-panel/src/trainer/controls/SliderControl.tsx
@@ -0,0 +1,18 @@
+import { type FormEvent } from 'react';
+
+import { numericValue } from './format-number';
+import { SliderReadout, type ControlInternalProps } from './shared';
+
+export const SliderControl = ({ cheat, value, disabled, onChange }: ControlInternalProps) => {
+  const min = cheat.args.min ?? 0;
+  const max = cheat.args.max ?? 100;
+  const step = cheat.args.step ?? 1;
+  const currentValue = numericValue(value, min);
+  const handleInput = (event: FormEvent<HTMLInputElement>) => onChange(Number(event.currentTarget.value));
+
+  return (
+    <div className="w-full">
+      <SliderReadout disabled={disabled} label={cheat.name} max={max} min={min} postfix={cheat.args.postfix ?? ''} step={step} value={currentValue} onInput={handleInput} />
+    </div>
+  );
+};
